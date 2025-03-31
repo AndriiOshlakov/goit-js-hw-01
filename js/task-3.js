b2790fae822678ac12a8594f583469ddc4ad2ec6
Andrii <andreyoshlakov18051986@gmail.com>
2025-03-31 11:12:02 +0300

    changing code

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,3 @@
 function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding) * 2;
-  const borderWidth = parseFloat(border) * 2;
-
-  return contentWidth + paddingWidth + borderWidth;
+  return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
 }
